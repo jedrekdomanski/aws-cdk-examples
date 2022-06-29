791ac3440c89ac31afbb496f081777586082a798
Jędrek Domański <jedrek.domanski@gmail.com>
2022-06-29 21:14:07 +0200

    Add new project Ec2NatInstance

---
+++ ec2-nat-instance/test/ec2-nat-instance.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ec2NatInstance from '../lib/ec2-nat-instance-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ec2-nat-instance-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ec2NatInstance.Ec2NatInstanceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
